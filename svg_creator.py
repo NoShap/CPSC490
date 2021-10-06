a1c98c6505bd64f6aa14cae6ec24f9f3f01fc55d
Noah Shapiro <noahshapiro@Noahs-MacBook-Pro.local>
2021-10-06 11:55:10 -0400

    edited draw_points function and added helper functions

--- svg_creator.py
+++ svg_creator.py
@@ -6,18 +6,25 @@ f = open("plane.json")
 sketch = json.load(f)
 
 
-# Takes in a set of points from a
-def draw_points(point_set):
-	my_drawing = svgwrite.drawing.Drawing(filename='noname.svg', size=('100%', '100%'))
+# Takes in a set of points and saves an svg in this file's directory
+def draw_points(point_set, color="black", svg_name="noname.svg"):
+	my_drawing = svgwrite.drawing.Drawing(filename=svg_name, size=('100%', '100%'))
 
-	for point in range(0,100):
-	the_line = my_drawing.line(start=(0, 0), end=(100, 100), stroke="red")
-	# the_line = my_drawing.polyline(points=[point_set])
-	my_drawing.add(the_line)
-	my_drawing.saveas("noname.svg")
+	for i in range(0, len(point_set-1)):
+		x1 = point_set[i]["x"]
+		y1 = point_set[i]["y"]
+		x2 = point_set[i+1]["x"]
+		y2 = point_set[i+1]["y"]
+		line = my_drawing.line(start=(x1, y1), end=(x2, y2), stroke=color)
+		my_drawing.add(line)
+		my_drawing.saveas(svg_name)
 
 
+def stroke_to_points(stroke):
+	return stroke["points"]
 
+def planes_to_strokes(plane):
+	return plane["strokes"]
 
 # For each plane in the JSON object, draw it's points onto our output file
 for pl in sketch["planes"]: # Each json file has a list of planes...
@@ -25,7 +32,12 @@ for pl in sketch["planes"]: # Each json file has a list of planes...
 		pass # print(stroke)		   # one of those objects is the strokes, which each have a list of points
 
 
-# print(len(sketch["planes"][0]["strokes"]))
+
+for stroke in sketch["planes"][0]["strokes"]:
+	for point in stroke["points"]:
+		print(point)
+	print("Total points in stroke:", i)
+
 
 draw_points([])
 
