b40d3a2b6907dbcc929cee19a1b6f31b1b10807f
Noah Shapiro <noahshapiro@Noahs-MacBook-Pro.local>
2021-10-06 12:53:37 -0400

    svg not saving multiple strokes

--- svg_creator.py
+++ svg_creator.py
@@ -2,22 +2,22 @@ import json
 import svgwrite
 
 # Fetch the Json object
-f = open("plane.json")
+f = open("single_plane.json")
 sketch = json.load(f)
 
 
 # Takes in a set of points and saves an svg in this file's directory
-def draw_points(point_set, color="black", svg_name="noname.svg"):
-	my_drawing = svgwrite.drawing.Drawing(filename=svg_name, size=('100%', '100%'))
+def draw_points(point_set, color="black", svg_name="new_sketch", scale=50, translate_x=10, translate_y=10):
+	my_drawing = svgwrite.drawing.Drawing(filename=svg_name + ".svg", size=('100%', '100%'))
 
-	for i in range(0, len(point_set-1)):
-		x1 = point_set[i]["x"]
-		y1 = point_set[i]["y"]
-		x2 = point_set[i+1]["x"]
-		y2 = point_set[i+1]["y"]
+	for i in range(0, len(point_set)-1):
+		x1 = point_set[i]["x"]* scale + translate_x
+		y1 = point_set[i]["y"] * scale + translate_y
+		x2 = point_set[i+1]["x"] * scale + translate_x
+		y2 = point_set[i+1]["y"] * scale + translate_y
 		line = my_drawing.line(start=(x1, y1), end=(x2, y2), stroke=color)
 		my_drawing.add(line)
-		my_drawing.saveas(svg_name)
+		my_drawing.saveas(svg_name + ".svg")
 
 
 def stroke_to_points(stroke):
@@ -26,21 +26,25 @@ def stroke_to_points(stroke):
 def planes_to_strokes(plane):
 	return plane["strokes"]
 
+def draw_sketch(sketch):
+	pass
 # For each plane in the JSON object, draw it's points onto our output file
 for pl in sketch["planes"]: # Each json file has a list of planes...
 	for stroke in pl["strokes"]:		   # which each have a list of objects...
 		pass # print(stroke)		   # one of those objects is the strokes, which each have a list of points
 
 
+draw_points(sketch["planes"][0]["strokes"][0]["points"], "black", "new_svg", 30, 20, 20)
+
+
+
 
 for stroke in sketch["planes"][0]["strokes"]:
-	for point in stroke["points"]:
-		print(point)
-	print("Total points in stroke:", i)
-
-
-draw_points([])
-
+	stroke_points = stroke_to_points(stroke)
+	draw_points(stroke_points)
+	
+		
+#Rotation Matrices to be applied!
 
 # rotate-z = 
 
