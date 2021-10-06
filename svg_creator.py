a3b789f0ba793e96bf72987f469be005c1751fee
Noah Shapiro <noahshapiro@Noahs-MacBook-Pro.local>
2021-10-06 11:36:26 -0400

    updated svg_creator to be able to draw lines

--- svg_creator.py
+++ svg_creator.py
@@ -10,15 +10,12 @@ sketch = json.load(f)
 def draw_points(point_set):
 	my_drawing = svgwrite.drawing.Drawing(filename='noname.svg', size=('100%', '100%'))
 
-	for i in range(0,100):
-		the_line = my_drawing.line(start=(i, i), end=(i+1, i+1))
-		# the_line = my_drawing.polyline(points=[point_set])
-		my_drawing.add(the_line)
-		my_drawing.saveas("noname.svg")
+	for point in range(0,100):
+	the_line = my_drawing.line(start=(0, 0), end=(100, 100), stroke="red")
+	# the_line = my_drawing.polyline(points=[point_set])
+	my_drawing.add(the_line)
+	my_drawing.saveas("noname.svg")
 
-	# circle = my_drawing.circle(center=(100, 100), r=100)
-	# my_drawing.add(circle)
-	# my_drawing.saveas("noname.svg")
 
 
 
@@ -31,3 +28,24 @@ for pl in sketch["planes"]: # Each json file has a list of planes...
 # print(len(sketch["planes"][0]["strokes"]))
 
 draw_points([])
+
+
+# rotate-z = 
+
+# [cos φ − sin φ 0
+# sin φ cos φ    0
+# 0 	   0	   1]
+
+# rotate-x = 
+
+# [1   0   0 
+# 0 cosφ -sinφ
+# o sinφ  cos φ]
+
+
+# rotate-y = 
+
+# [cosφ   0   sinφ 
+# 0      1    0
+# -sinφ   0 cos φ]
+
